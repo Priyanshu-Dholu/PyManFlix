f7b2c81fb7fddbe4c293c98514d877d0abb610e8
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-06-14 20:19:05 +0530

    Added Options To Update/Delete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -11,7 +11,7 @@ db = SQLAlchemy(app)
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), unique=True)
-    year = db.Column(db.Integer)    
+    year = db.Column(db.Integer)
     link = db.Column(db.String(500))
     added_on = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -38,14 +38,34 @@ def hello_world():
     all_movies = Movies.query.all()
     print(all_movies)    
     return render_template('add_movies.html',all_movies=all_movies)
-    
-    
-# Just An Empty Page
-@app.route('/show')
-def show():
+
+# Delete Movie Function
+@app.route('/delete/<int:id>')
+def delete(id):
+    movie = Movies.query.filter_by(id=id).first()
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(location='/')
+
+# Delete Movie Page
+@app.route('/delete_movie')
+def delete_movie():
     all_movies = Movies.query.all()
-    print(all_movies)
-    return 'this is movie page'
+    return render_template('delete_movie.html',all_movies=all_movies)
+    
+# Update Movie Function
+@app.route('/update/<int:id>', methods = ['GET','POST'])
+def update(id):
+    movie = Movies.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        movie.title = request.form['title']
+        movie.year = request.form['year']
+        movie.link = request.form['link']
+        db.session.commit()
+        return redirect(location='/')
+    
+    return render_template('update_movie.html',movie=movie)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
