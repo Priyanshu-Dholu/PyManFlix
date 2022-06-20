704baeb8c68021c9d4c23bdc07e88779b8eed371
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-06-20 18:35:38 +0530

    Modified as Per Postgress

--- manflix/models.py
+++ manflix/models.py
@@ -4,18 +4,17 @@ from manflix import db
 # Movie Database
 class Movies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True)
+    title = db.Column(db.varchar(80), unique=True)
     year = db.Column(db.Integer)
-    link = db.Column(db.String(500))
-    quality = db.Column(db.Boolean)
+    link = db.Column(db.varchar(500))
+    quality = db.Column(db.boolean)
     movie_id_tmd = db.Column(db.Integer)
-    poster_link = db.Column(db.String(500))
-    movie_backdrop_link = db.Column(db.String(500))
-    movie_overview = db.Column(db.String(500))
-    trailer_link = db.Column(db.String(125))
-    dolby_audio = db.Column(db.Boolean)
-    dual_audio = db.Column(db.Boolean)
-    added_on = db.Column(db.DateTime, default=datetime.utcnow)
+    poster_link = db.Column(db.varchar(500))
+    movie_backdrop_link = db.Column(db.varchar(500))
+    movie_overview = db.Column(db.varchar(500))
+    trailer_link = db.Column(db.varchar(125))
+    dolby_audio = db.Column(db.boolean)
+    dual_audio = db.Column(db.boolean)
 
     def __repr__(self) -> str:
-        return f"movies('{self.title}','{self.year}','{self.link}','{self.added_on},{self.quality}','{self.dolby_audio}','{self.dual_audio},{self.poster_link},{self.movie_id_tmd}','{self.movie_backdrop_link}','{self.movie_overview}','{self.trailer_link}')"
+        return f"movies('{self.title}','{self.year}','{self.link}',{self.quality}','{self.dolby_audio}','{self.dual_audio},{self.poster_link},{self.movie_id_tmd}','{self.movie_backdrop_link}','{self.movie_overview}','{self.trailer_link}')"
