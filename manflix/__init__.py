4216f5543cd2f90feeff4bb6f0a1e20b8419b6e7
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-06-21 11:05:48 +0530

    Removed ENV FlaskENV

--- manflix/__init__.py
+++ manflix/__init__.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tagqsilmewdzer:bf7cf5abc49f1c48d9bf92f7a70fab51662863b1fa4a74961e47b05460ebe246@ec2-52-214-23-110.eu-west-1.compute.amazonaws.com:5432/ddm4jsmvs45cdf'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = '592dc58d1229ead98e47754f4d744767'
 db = SQLAlchemy(app)
 
 from manflix import routes
