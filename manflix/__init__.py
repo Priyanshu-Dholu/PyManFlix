f016181de8d53272e5b08e3abe118d51f3c68b61
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-06-21 11:09:55 +0530

    Removed Loadenv

--- manflix/__init__.py
+++ manflix/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-load_dotenv()
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tagqsilmewdzer:bf7cf5abc49f1c48d9bf92f7a70fab51662863b1fa4a74961e47b05460ebe246@ec2-52-214-23-110.eu-west-1.compute.amazonaws.com:5432/ddm4jsmvs45cdf'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
