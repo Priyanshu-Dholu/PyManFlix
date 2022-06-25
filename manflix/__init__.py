23dab930b6ebbeffa35e0007091b8b20da383eca
Priyanshu Dholu <patelpriyanshu649@gmail.com>
2022-06-25 17:34:39 +0530

    Added Flask-Login

--- manflix/__init__.py
+++ manflix/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 import os
 
 app = Flask(__name__)
@@ -13,5 +14,4 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-
 from manflix import routes
